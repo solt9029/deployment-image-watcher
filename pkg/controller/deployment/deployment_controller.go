71cf369abd311d4f434c6fb5680f989eb51c6394
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-02-16 13:03:08 +0900

    add watch and slack

--- pkg/controller/deployment/deployment_controller.go
+++ pkg/controller/deployment/deployment_controller.go
@@ -1,42 +1,65 @@
 package deployment
 
 import (
+	"os"
+
 	"github.com/nlopes/slack"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 var log = logf.Log.WithName("controller_deployment")
 
-// Add creates a new Deployment Controller and adds it to the Manager. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
 }
 
-// newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 	return &ReconcileDeployment{client: mgr.GetClient(), scheme: mgr.GetScheme()}
 }
 
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
 	c, err := controller.New("deployment-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource Deployment
-	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForObject{})
+	slackToken := os.Getenv("SLACK_TOKEN")
+	slackChannel := os.Getenv("SLACK_CHANNEL")
+	slackClient := slack.New(slackToken)
+
+	source := &source.Kind{Type: &appsv1.Deployment{}}
+	handler := &handler.EnqueueRequestForObject{}
+	predicate := predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			oldContainers := e.MetaOld.(*appsv1.Deployment).Spec.Template.Spec.Containers
+			newContainers := e.MetaNew.(*appsv1.Deployment).Spec.Template.Spec.Containers
+
+			for oldIndex := 0; oldIndex < len(oldContainers); oldIndex++ {
+				oldContainer := oldContainers[oldIndex]
+				for newIndex := 0; newIndex < len(newContainers); newIndex++ {
+					newContainer := newContainers[newIndex]
+					if oldContainer.Name == newContainer.Name && oldContainer.Image != newContainer.Image {
+						messageText := createMessage(newContainer.Name, newContainer.Image, oldContainer.Image)
+						slackClient.PostMessage(slackChannel, slack.MsgOptionText(messageText, true))
+					}
+				}
+			}
+
+			return true
+		},
+	}
+
+	err = c.Watch(source, handler, predicate)
 	if err != nil {
 		return err
 	}
@@ -52,11 +75,11 @@ type ReconcileDeployment struct {
 }
 
 func (r *ReconcileDeployment) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling")
-
-	api := slack.New(os.Getenv("SLACK_BOT_USER_OAUTH_ACCESS_TOKEN"))
-	api.PostMessage("popper", slack.MsgOptionText("🎉", true))
-
 	return reconcile.Result{}, nil
+}
+
+func createMessage(name string, newImage string, oldImage string) string {
+	return "[ " + oldImage + " => " + newImage + " ]" + " " +
+		"deployment image updated" + " " +
+		"( " + "deployment: " + name + ", " + "container: " + name + " )"
 }
